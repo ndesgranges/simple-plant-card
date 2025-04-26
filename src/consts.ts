ba1b6a005aa037c33a6607ca01e7cff7ada07927
ndesgranges <ndesgranges>
2025-04-26 12:26:00 +0000

    Updated version for release

--- src/consts.ts
+++ src/consts.ts
@@ -4,6 +4,6 @@ export const CARD_TYPE="simple-plant-card";
 export const CARD_NAME="Simple Plant Card";
 export const CARD_DESCRIPTION="Custom card for simple-plant integration";
 
-export const CARD_VERSION="v1.2.0";
+export const CARD_VERSION="v1.2.1";
 export const CARD_AUTHOR="ndesgranges";
 
